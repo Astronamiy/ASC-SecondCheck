822334fab12e67c09673d57837cbb178b4b5a0dc
wangziyang <305885331@qq.com>
2025-11-04 17:36:00 +0800

    main文件更新

--- User/main.c
+++ User/main.c
@@ -3,28 +3,14 @@
 #include "Encoder.h"
 #include "OLED.h"
 #include "Timer.h"
-
-int Speed;
+#include "Delay.h"
+#include "Serial.h"
 
 int main()
 {
-	Motor_Init();
-	Encoder_Init();
-	OLED_Init();
-	Timer_Init();
-	Motor_SetSpeed1(-0);
-	Motor_SetSpeed2(-20);
 	while(1)
 	{
-		OLED_ShowSignedNum(1 ,1 ,Speed ,5);
+		
 	}
 }
 
-void TIM2_IRQHandler(void)
-	{
-		if (TIM_GetITStatus(TIM2 ,TIM_IT_Update) == SET)
-		{
-			Speed = Encoder_Get();
-			TIM_ClearITPendingBit(TIM2 ,TIM_IT_Update);
-		}
-	}
